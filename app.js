f7bdf42018b96b3eda09f478596e0052f6850a05
Cesar Maquera <yushiroche@gmail.com>
2018-01-06 08:10:33 +0000

    primer commit del chatbot

--- app.js
+++ app.js
@@ -30,27 +30,6 @@ app.get("/webhook", function (req, res) {
     }
 });
 
-// Accepts POST requests at /webhook endpoint
-app.post('/webhook', (req, res) => {  
-    // Parse the request body from the POST
-    let body = req.body;
-    // Check the webhook event is from a Page subscription
-    if (body.object === 'page') {
-    // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-        // Get the webhook event. entry.messaging is an array, but 
-        // will only ever contain one event, so we get index 0
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
-    });
-        // Return a '200 OK' response to all events
-        res.status(200).send('EVENT_RECEIVED');
-    } else {
-        // Return a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
-    }
-});
-
 // Todos eventos de mesenger sera apturados por esta ruta
 app.post("/webhook", function (req, res) {
     // Verificar si el vento proviene del pagina asociada
@@ -60,7 +39,7 @@ app.post("/webhook", function (req, res) {
             // Iterara todos lo eventos capturados
             entry.messaging.forEach(function(event) {
                 if (event.message) {
-                  process_event(event);
+                    process_event(event);
                 }
             });
         });
